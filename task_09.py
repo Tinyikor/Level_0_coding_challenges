b268c5bde7619a13b5b1ba9eec6159695fe2a452
Tinyikor <91489781+Tinyikor@users.noreply.github.com>
2022-01-14 14:12:59 +0200

    nineth task commit

---
+++ task_09.py
@@ -0,0 +1,12 @@
+def UniqueVowels(string):
+    vowels = ("aeiou")
+    result = ""
+    for letter in string:
+        letter = letter.lower()
+        if ( letter in vowels ) and (letter not in result ):
+            result += letter
+    print ("Vowels: ", end="")
+    print (*result, sep=",")
+
+
+UniqueVowels("Umuzi")
